fe7a78b38d716548fe2cbf15c71fcfa7095e4ebf
xPrismatico <srfuentesavila@gmail.com>
2024-11-17 21:35:25 -0300

    fix: models error fixed

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2042e6c2c5f0488059d3d48b3cbbe2d8a53aa7ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c75a5e164378b61921dd7d760f930c0428cc97")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
