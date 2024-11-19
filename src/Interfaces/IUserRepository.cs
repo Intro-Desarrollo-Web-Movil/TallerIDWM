a4765589d168aa34fdee24270d3762e738cc13fa
Axel <axel.mondaca@alumnos.ucn.cl>
2024-11-19 07:33:46 -0300

    fix: fixed user interfaces and mappers

--- src/Interfaces/IUserRepository.cs
+++ src/Interfaces/IUserRepository.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TallerIDWM.src.DTOs;
 
 namespace TallerIDWM.src.Interfaces
 {
-    public class IUserRepository
+    public interface IUserRepository
     {
-        
+        Task<UserDto> CreateUser(CreateUserDto createUserDto);
+        Task<UserDto> DeleteUser (int id);
+        Task<UserDto?> GetUserById(int id);
+        Task<UserDto> UpdateUser(int id, UpdateUserDto updateUserDto);
     }
 }
--- src/Mappers/UserMapper.cs
+++ src/Mappers/UserMapper.cs
@@ -2,11 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TallerIDWM.src.DTOs;
+using TallerIDWM.src.Models;
 
 namespace TallerIDWM.src.Mappers
 {
-    public class UserMapper
+    public static class UserMapper
     {
-        
+        public static UserDto toUserDto (this User user){
+            return new UserDto {
+                UserId = user.UserId,
+                Name = user.Name,
+                Email = user.Email,
+                Password = user.Password,
+                BirthDate = user.BirthDate,
+                IsActive = user.IsActive,
+                RoleId = user.RoleId,
+                role = user.role,
+                GenderId = user.GenderId,
+                Gender = user.Gender
+            };
+        }
+        public static User toUser (this UserDto userDto){
+            return new User {
+                UserId = userDto.UserId,
+                Name = userDto.Name,
+                Email = userDto.Email,
+                Password = userDto.Password,
+                BirthDate = userDto.BirthDate,
+                IsActive = userDto.IsActive,
+                RoleId = userDto.RoleId,
+                role = userDto.role,
+                GenderId = userDto.GenderId,
+                Gender = userDto.Gender
+            };
+        }
     }
 }
